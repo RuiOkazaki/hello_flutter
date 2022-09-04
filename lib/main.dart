07639526a0529eab94be8f8cda017aa8e37b32ec
OkazakiRui <okazaki200226@gmail.com>
2022-09-04 17:37:23 +0900

    refactor: ファイル分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_app/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,87 +23,6 @@ class MyApp extends StatelessWidget {
       // darkThemeを先程宣言したdarkThemeにする
       darkTheme: darkTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // 関数を宣言
-  void _incrementCounter() {
-    // _counterのsetterで値をincrementしている
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // レイアウトを構築するためのwidget（Scaffold）
-    final row = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: const [
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-      ],
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            const SizedBox(
-                height: 60, width: 60, child: ColoredBox(color: Colors.green)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("ほげり"),
-                Text("ふがり"),
-              ],
-            ),
-            // Expanded()
-            // shrinkWrap: true,
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: GridView.count(crossAxisCount: 3, children: [
-                for (var i = 0; i < 9; i++)
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: ColoredBox(color: Colors.indigo),
-                  )
-              ]),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
