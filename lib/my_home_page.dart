665a84adc219c08b7da4b4180d2d25f878941c1d
OkazakiRui <okazaki200226@gmail.com>
2022-09-04 17:39:46 +0900

    refactor: コード整理

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -23,14 +23,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // レイアウトを構築するためのwidget（Scaffold）
-    final row = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: const [
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-        SizedBox(width: 60, height: 60, child: ColoredBox(color: Colors.teal)),
-      ],
-    );
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -38,7 +30,6 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
@@ -46,28 +37,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
-            const SizedBox(
-                height: 60, width: 60, child: ColoredBox(color: Colors.green)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text("ほげり"),
-                Text("ふがり"),
-              ],
-            ),
-            // Expanded()
-            // shrinkWrap: true,
-            SizedBox(
-              width: 200,
-              height: 200,
-              child: GridView.count(crossAxisCount: 3, children: [
-                for (var i = 0; i < 9; i++)
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: ColoredBox(color: Colors.indigo),
-                  )
-              ]),
             ),
           ],
         ),
